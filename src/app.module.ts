ab455c1727d32a5cfaa4fc28552f355fd0f7026d
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-04-06 13:25:52 -0600

    feat: add logger database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarModule } from './modules/cars/car.module';
-import { PrismaService } from './orm';
 import { RentalsModule } from './modules/rentals/rentals.module';
 
 @Module({
   imports: [CarModule, RentalsModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
